bf8aeadfe3b8a4f96013cb50227aab7c5932f9c5
Azim Momin <azimmomin123@gmail.com>
2016-12-12 18:18:07 -0600

    added another .get function

--- example.js
+++ example.js
@@ -9,10 +9,14 @@ var easypost = require('node-easypost')(apiKey); // after installing with NPM th
 
 
 // This allows Cross-Origin Requests to our server
-app.use(function(req, res, next) {
+app.use(function(request, response, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/', function(request, response){
+    response.send("Hello world!");
 });
 
 app.get('/:length/:width/:height/:weight', function (request, response) {
