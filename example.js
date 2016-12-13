55954a10d0a764113737bfca5142c9ee8d7ec4f5
Azim Momin <azimmomin123@gmail.com>
2016-12-12 19:01:16 -0600

    added two variables

--- example.js
+++ example.js
@@ -9,11 +9,11 @@ var easypost = require('node-easypost')(apiKey); // after installing with NPM th
 
 
 // This allows Cross-Origin Requests to our server
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // app.get('/', function(request, response){
 //     response.send("Hello world!");
